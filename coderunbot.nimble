fa21f0aaec27817b767750191a24f4701bcfdb11
Gart2357 <gart@outlook.jp>
2020-09-22 19:16:40 +0900

    test と echo コマンド

---
+++ coderunbot.nimble
@@ -0,0 +1,14 @@
+# Package
+
+version       = "0.1.0"
+author        = "Gart2357"
+description   = "Runnig your code on Discord"
+license       = "AGPL-3.0"
+srcDir        = "src"
+bin           = @["coderunbot"]
+
+
+
+# Dependencies
+
+requires "nim >= 1.2.0", "dimscord >= 1.0.0", "dotenv >= 1.1.0"
---
+++ src/coderunbot.nim
@@ -0,0 +1,42 @@
+import dimscord, asyncdispatch, strutils, options, dotenv, os
+
+let env = initDotEnv()
+env.load()
+
+let discord = newDiscordClient(getEnv("DISCORD_TOKEN"))
+
+proc messageCreate(s: Shard, m: Message) {.async.} =
+  let args = m.content.split(" ")
+  if m.author.bot or not args[0].startsWith("]"):
+    return
+  let command = args[0][1..args[0].high]
+
+  case command.toLowerAscii():
+  of "test":
+    discard await discord.api.sendMessage(m.channel_id, "Success!")
+  of "echo":
+    var text = args[1..args.high].join(" ")
+    if text == "":
+      text = "Empty text."
+    discard await discord.api.sendMessage(m.channel_id, text)
+  else:
+    discard
+
+proc onReady(s: Shard, r: Ready) {.async.} =
+  echo "Ready as: " & $r.user
+
+  await s.updateStatus(game = some GameStatus(
+    name: "around.",
+    kind: gatPlaying
+  ), status = "idle")
+
+proc messageDelete(s: Shard, m: Message, exists: bool) {.async.} =
+  echo "Awild message has been deleted!"
+
+discord.events.onReady = onReady
+discord.events.messageCreate = messageCreate
+discord.events.messageDelete = messageDelete
+
+# Connect to Discord and run the bot.
+when isMainModule:
+  waitFor discord.startSession()
