e65f7db3ae4660ce87be6d7c8feeb68b7f50c43f
Gart2357 <gart@outlook.jp>
2020-12-14 10:37:58 +0900

    自前でレンダリング

--- commands/command_tex.py
+++ commands/command_tex.py
@@ -16,18 +16,27 @@ async def main(message, arg):
     with open(f'{here}/tex_template/tex.tex', 'r') as f:
         tex_con = f.read().replace('[REPLACE]', arg.strip())
 
-    cp = subprocess.run(["tex2jpg"], input=tex_con.encode('UTF-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    with open(f'/tmp/' + fid + '.tex', 'w') as f:
+        f.write(tex_con)
 
-    if cp.returncode != 0:
+    _ = subprocess.run(['uplatex', '-halt-on-error', '-output-directory=/tmp', '/tmp/' + fid + '.tex'])
+    dvipdfmx = subprocess.run(['dvipdfmx', '-q', '-o', '/tmp/' + fid + '.pdf', '/tmp/' + fid + '.dvi'])
+
+    if dvipdfmx.returncode != 0:
+        with open('/tmp/' + fid + '.log', 'r') as f:
+            err = f.read().split('!')[1].split('Here')[0]
         embed = discord.Embed(
             title='エラー Error',
-            description=f'```\n{cp.stderr.decode("UTF-8")}\n```',
+            description=f'```\n{err}\n```',
             color=0xff0000
-        )
+            )
         embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
         return await message.channel.send(embed=embed)
-    else:
-        embed = discord.Embed(color=0x008000)
-        embed.set_image(url=f'attachment://tex.jpg')
-        embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
-        return await message.channel.send(file=discord.File(io.BytesIO(cp.stdout), filename="tex.jpg"), embed=embed)
+
+    _ = subprocess.run(['pdfcrop', '/tmp/' + fid + '.pdf', '--margins', '4 4 4 4'])
+    pdftoppm = subprocess.run(['pdftoppm', '-png', '-r', '800', '/tmp/' + fid + '-crop.pdf'], stdout=subprocess.PIPE)
+
+    embed = discord.Embed(color=0x008000)
+    embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+    embed.set_image(url=f'attachment://tex.png')
+    return await message.channel.send(file=discord.File(io.BytesIO(pdftoppm.stdout), filename='tex.png'), embed=embed)
