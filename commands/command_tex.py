9018f456c288fc3c255c16674e57706449d39596
Gart2357 <gart@outlook.jp>
2021-01-12 13:05:53 +0900

    fix tex

--- commands/command_tex.py
+++ commands/command_tex.py
@@ -18,7 +18,7 @@ async def main(message, arg):
     
     if '\\input' in tex_con or '\\include' in tex_con or '\\csname' in tex_con:
         embed = discord.Embed(
-            title='使用できない文字列が含まれています\\Contains a string that cannot be used',
+            title='使用できない文字列が含まれています',
             color=0xff0000
         )
         embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
@@ -28,28 +28,29 @@ async def main(message, arg):
         f.write(tex_con)
 
     try:
-        _ = subprocess.run(['uplatex', '-halt-on-error', '-output-directory=/tmp', '/tmp/' + fid + '.tex'], timeout=5)
-        dvipdfmx = subprocess.run(['dvipdfmx', '-q', '-o', '/tmp/' + fid + '.pdf', '/tmp/' + fid + '.dvi'], timeout=5)
+        uplatex = subprocess.run(['uplatex', '-halt-on-error', '-output-directory=/tmp', '/tmp/' + fid + '.tex'], timeout=10)
     except subprocess.TimeoutExpired:
         embed = discord.Embed(
-            title='タイムアウト Timeout',
+            title='タイムアウト',
             color=0xff0000
         )
         embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
         return await message.channel.send(embed=embed)
 
-    if dvipdfmx.returncode != 0:
+    if uplatex.returncode != 0:
         with open('/tmp/' + fid + '.log', 'r') as f:
             err = f.read().split('!')[1].split('Here')[0]
+        subprocess.run(f'rm /tmp/{fid}.*', shell=True)
         embed = discord.Embed(
-            title='エラー Error',
+            title='レンダリングエラー',
             description=f'```\n{err}\n```',
             color=0xff0000
         )
         embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
         return await message.channel.send(embed=embed)
-    
-    _ = subprocess.run(['pdfcrop', '/tmp/' + fid + '.pdf', '--margins', '4 4 4 4'])
+
+    subprocess.run(['dvipdfmx', '-q', '-o', '/tmp/' + fid + '.pdf', '/tmp/' + fid + '.dvi'], timeout=10)
+    subprocess.run(['pdfcrop', '/tmp/' + fid + '.pdf', '--margins', '4 4 4 4'])
     pdftoppm = subprocess.run(['pdftoppm', '-png', '-r', '800', '/tmp/' + fid + '-crop.pdf'], stdout=subprocess.PIPE)
 
     embed = discord.Embed(color=0x008000)
--- commands/command_texp.py
+++ commands/command_texp.py
@@ -13,12 +13,12 @@ async def main(message, arg):
     fid = str(random.SystemRandom().randint(10000, 99999))
     here = os.path.dirname(__file__)
 
-    with open(f'{here}/tex_template/tex.tex', 'r') as f:
+    with open(f'{here}/tex_template/texp.tex', 'r') as f:
         tex_con = f.read().replace('[REPLACE]', arg.strip())
-    
+        
     if '\\input' in tex_con or '\\include' in tex_con or '\\csname' in tex_con:
         embed = discord.Embed(
-            title='使用できない文字列が含まれています\\Contains a string that cannot be used',
+            title='使用できない文字列が含まれています',
             color=0xff0000
         )
         embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
@@ -28,33 +28,36 @@ async def main(message, arg):
         f.write(tex_con)
 
     try:
-        _ = subprocess.run(['uplatex', '-halt-on-error', '-output-directory=/tmp', '/tmp/' + fid + '.tex'], timeout=5)
-        dvipdfmx = subprocess.run(['dvipdfmx', '-q', '-o', '/tmp/' + fid + '.pdf', '/tmp/' + fid + '.dvi'], timeout=5)
+        uplatex = subprocess.run(['uplatex', '-halt-on-error', '-output-directory=/tmp', '/tmp/' + fid + '.tex'], timeout=10)
     except subprocess.TimeoutExpired:
         embed = discord.Embed(
-            title='タイムアウト Timeout',
+            title='タイムアウト',
             color=0xff0000
         )
         embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
         return await message.channel.send(embed=embed)
 
-    if dvipdfmx.returncode != 0:
+    if uplatex.returncode != 0:
         with open('/tmp/' + fid + '.log', 'r') as f:
             err = f.read().split('!')[1].split('Here')[0]
+        subprocess.run(f'rm /tmp/{fid}.*', shell=True)
         embed = discord.Embed(
-            title='エラー Error',
+            title='レンダリングエラー',
             description=f'```\n{err}\n```',
             color=0xff0000
         )
         embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
         return await message.channel.send(embed=embed)
-    
-    _ = subprocess.run(['pdfcrop', '/tmp/' + fid + '.pdf', '--margins', '4 4 4 4'])
+
+    subprocess.run(['dvipdfmx', '-q', '-o', '/tmp/' + fid + '.pdf', '/tmp/' + fid + '.dvi'], timeout=10)
+    subprocess.run(['pdfcrop', '/tmp/' + fid + '.pdf', '--margins', '4 4 4 4'])
     pdftoppm = subprocess.run(['pdftoppm', '-png', '-r', '800', '/tmp/' + fid + '-crop.pdf'], stdout=subprocess.PIPE)
     convert1 = subprocess.run(['convert', '-', '-gravity', 'northwest', '-chop', '0x150', 'png:-'], input=pdftoppm.stdout, stdout=subprocess.PIPE)
     convert2 = subprocess.run(['convert', '-', '-gravity', 'southeast', '-chop', '0x200', 'png:-'], input=convert1.stdout, stdout=subprocess.PIPE)
     convert3 = subprocess.run(['convert', '-', '-background', '#FFFFFF', '-gravity', 'northwest', '-splice', '10x10', 'png:-'], input=convert2.stdout, stdout=subprocess.PIPE)
     convert4 = subprocess.run(['convert', '-', '-background', '#FFFFFF', '-gravity', 'southeast', '-splice', '10x10', 'png:-'], input=convert3.stdout, stdout=subprocess.PIPE)
+
+    subprocess.run(f'rm /tmp/{fid}.*', shell=True)
 
     embed = discord.Embed(color=0x008000)
     embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
