1afcb33de51bfbf48f6dacd8112883d7d6a43a1f
Gart <57865205+Gart2357@users.noreply.github.com>
2021-03-25 23:41:51 +0900

    addd comment

--- coderunbot.py
+++ coderunbot.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import asyncio
 import os
 import traceback
@@ -12,6 +11,7 @@ import discord
 from config import DISCORD_TOKEN, PREFIX, SERVER_URL, INVITE_URL
 
 
+# dictionary to adjust size automatically
 class LimitedSizeDict(OrderedDict):
 
 
@@ -35,31 +35,33 @@ class LimitedSizeDict(OrderedDict):
 
 client = discord.Client()
 
-message_id_to_author_id = LimitedSizeDict(size_limit=100)
+# save the author of the message the bot sent
+message_id_to_author_id = LimitedSizeDict(size_limit=100
+# link user's message to the bot's message
 user_message_id_to_bot_message = LimitedSizeDict(size_limit=100)
 
+                                          
 @client.event
 async def on_ready():
-    print('起動しました')
+    print('It\'s activated')
 
 
 @client.event
 async def on_message(message):
-
     await reply(message)
-    if 'にゃーん' in message.content:
-        await message.add_reaction('😿')
 
 
 @client.event
 async def on_message_edit(before, after):
 
+    # if the sent message is a call to the bot
     if before.id in user_message_id_to_bot_message:
         try:
+            # delete the bot message
             await globals()['user_message_id_to_bot_message'][before.id].delete()
         except discord.errors.NotFound:
             pass
-
+    # respond to the edited messege
     await reply(after)
 
 
@@ -69,16 +71,20 @@ async def on_reaction_add(reaction, user):
     if user == client.user:
         return
 
+    # if the reacted message is the bot's
+    # and the person who reacted is the person who typed the command
     if reaction.message.author == client.user \
         and reaction.message.id in message_id_to_author_id \
         and user.id == message_id_to_author_id[reaction.message.id]:
 
-        if str(reaction.emoji) =='🚮':
+        if str(reaction.emoji) in ('🚮', '✖️', '🗑️'):
             await reaction.message.delete()
 
 
+# respond to the sent command
 async def reply(message):
 
+    # if the author is a bot other than PythonBot and botphilia
     if message.author.bot:
         return
 
@@ -87,6 +93,7 @@ async def reply(message):
         command = message.content.split()[0][len(PREFIX):]
         arg = message.content[len(PREFIX) + len(command):].lstrip()
 
+        # if the command file exists atthe specified location
         if os.path.exists(f'{os.path.dirname(os.path.abspath(__file__))}/commands/command_{command}.py'):
 
             tmp_module = import_module(f'commands.command_{command}')
@@ -105,7 +112,9 @@ async def reply(message):
                     )
                     embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
                     sent_message = await message.channel.send(embed=embed)
+                # save the author of the message the bot sent
                 message_id_to_author_id[sent_message.id] = message.author.id
+                # link user's message to the bot's message
                 user_message_id_to_bot_message[message.id] = sent_message
                 await sent_message.add_reaction('🚮')
                 if random.randrange(100) == 0:
@@ -120,5 +129,6 @@ async def reply(message):
                         description=SERVER_URL
                     )
                     await message.channel.send(embed=embed)
+
 
 client.run(DISCORD_TOKEN)
