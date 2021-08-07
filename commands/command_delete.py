cb73385e80049aba819144dd9eded8c34f65d8b2
Gart2357 <gart@outlook.jp>
2021-08-07 17:50:25 +0900

    add delete command

---
+++ commands/command_delete.py
@@ -0,0 +1,31 @@
+import json
+import os
+import re
+
+import discord
+
+here = os.path.dirname(__file__)
+
+
+async def main(message: discord.Message, arg: str):
+
+    if os.path.exists(f'{here}/saved_codes/{message.author.id}.json'):
+        os.remove(f'{here}/saved_codes/{message.author.id}.json')
+        embed = discord.Embed(
+            title='削除しました Deleted',
+        )
+        embed.set_author(
+            name=message.author.name,
+            icon_url=message.author.avatar_url
+        )
+        return await message.reply(embed=embed)
+    else:
+        embed = discord.Embed(
+            title='保存されていません Not saved',
+            color=0xff0000,
+        )
+        embed.set_author(
+            name=message.author.name,
+            icon_url=message.author.avatar_url
+        )
+        return await message.reply(embed=embed)
